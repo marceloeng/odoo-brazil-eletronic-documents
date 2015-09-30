0d3814ce243e26bf0809eb605c0597d6fdc4ebce
marceloeng <marcelo@comdesk.com.br>
2015-09-30 15:47:22 -0300

    Adcionado botão 'Habilitado SEFAZ', que faz consulta sem retornar dados do cliente

--- nfe/res_partner.py
+++ nfe/res_partner.py
@@ -96,3 +96,55 @@ class ResPartner(orm.Model):
                 }
                 self.write(cr, uid, [partner.id], result, context)
         return
+    
+    # Verifica se est� habilitado no SEFAZ sem escrever os dados do cliente 
+    def sefaz_check_enable(self, cr, uid, ids, context=False):
+
+        if context.get('company_id', False):
+            company = context['company_id']
+        else:
+            company = self.pool.get('res.users').browse(
+                cr, uid, uid,
+                context=context
+            ).company_id
+
+        validate_nfe_configuration(company)
+
+        for partner in self.browse(cr, uid, ids, context):
+            if partner.cnpj_cpf:
+                cnpj_cpf = partner.cnpj_cpf
+
+                estato = partner.state_id.code or None
+                ie = partner.inscr_est or None
+                ie = ie if ie != 'ISENTO' else None
+
+                processo = check_partner(company, cnpj_cpf, estato, ie)
+                xml = processo.resposta.xml.encode('utf-8')
+
+                tree = ET.fromstring(xml)
+                info = {}
+
+                for child in tree:
+                    info[child.tag[36:]] = child.text
+                    for infCons in child:
+                        info[infCons.tag[36:]] = infCons.text
+                        for infCad in infCons:
+                            info[infCad.tag[36:]] = infCad.text
+                            for end in infCad:
+                                info[end.tag[36:]] = end.text
+
+                if info['cStat'] not in ('111', '112'):
+                    raise orm.except_orm(
+                        _("Erro ao se comunicar com o SEFAZ"),
+                        _("%s - %s") % (info.get('cStat', ''),
+                                        info.get('xMotivo', '')))
+                if info['cSit'] not in ('1',):
+                    raise orm.except_orm(
+                        _("Situação Cadastral Vigente:"),
+                        _("NÃO HABILITADO"))                
+
+                result = {                    
+                    'habilitado_sintegra': info['cSit'],
+                }
+                #self.write(cr, uid, [partner.id], result, context)
+        return result
